79b34c7e4692aca96463a3004ec6e6bcb727ac16
hakan efe <hakanforwork@gmail.com>
2023-03-05 17:54:38 +0200

    week 3

---
+++ Week3RemainderOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿//odd or even
+using System.Collections.Concurrent;
+
+int a = 11;
+
+int result = a % 2;
+
+Console.WriteLine(result);
+
+//if (result == 0) {
+//    Console.WriteLine($"{a} is an even number" );
+
+//}
+//else
+//{
+//    Console.WriteLine($"{a} is an odd number ");
+//}
+switch (result)
+{
+    case 0:
+        Console.WriteLine($"{a} is an even number");
+        break;
+   case 1:
+        Console.WriteLine($"{a} is an odd number");
+        break;
+    default:
+        Console.WriteLine("invalid value");
+        break;
+}
